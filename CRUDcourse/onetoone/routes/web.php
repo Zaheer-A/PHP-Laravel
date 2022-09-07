a414915de76fc2d48f3c3aa713fd5ebaf95efb81
Ashma <ashma@zaheer.com>
2022-09-07 12:45:40 +0200

    Complete Tutorial

--- CRUDcourse/onetoone/routes/web.php
+++ CRUDcourse/onetoone/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\User;
+use App\Models\Address;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,29 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/insert/{id}', function ($id){
+   $user = User::findOrFail($id);
+   $address = new Address(['name'=>'147 Brongouw 1352EN']);
+   $user->address()->save($address);
+});
+
+Route::get('/update', function(){
+    //get()->returns an array of results
+    //first() returns the first one
+    $address = Address::where('user_id', 1)->first();
+    $address->name = "123 Updated Avenue Alaska";
+    $address->save();
+    echo "Addresses added succesfully!";
+});
+
+Route::get('/read', function(){
+    $user = User::findOrFail(1);
+    echo $user->address->name;
+});
+
+Route::get('/delete', function(){
+    $user = User::findOrFail(1);
+    echo $user->address()->delete();
 });
