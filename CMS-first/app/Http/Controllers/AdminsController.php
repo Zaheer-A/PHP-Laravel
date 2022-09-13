01ec4bf089a8c0c5e18778d03d1f32acbf6a88c3
Ashma <ashma@zaheer.com>
2022-09-13 10:14:31 +0200

    Dummy data

--- CMS-first/app/Http/Controllers/AdminsController.php
+++ CMS-first/app/Http/Controllers/AdminsController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class AdminsController extends Controller
 {
-
+    public function index(){
+        return view('admin.index');
+    }
 }
--- CMS-first/database/seeders/DatabaseSeeder.php
+++ CMS-first/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Post;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+         \App\Models\User::factory()
+             ->count(2)
+             ->has(Post::factory()->count( 2), 'posts')
+             ->create();
     }
 }
